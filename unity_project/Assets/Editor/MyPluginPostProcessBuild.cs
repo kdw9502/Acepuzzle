64b0f51f9f37119fe5ae0f633d3c149fe7edd58d
kdw9502ACE <kdw9502@aceproject.com>
2018-04-04 18:44:07 +0900

    [빌드]  postbuild로 bit_code  설정 끄기

---
+++ unity_project/Assets/Editor/MyPluginPostProcessBuild.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.Collections;
+using UnityEditor.iOS.Xcode;
+using System.IO;
+using System.Collections.Generic;
+
+public class MyPluginPostProcessBuild
+{
+	[PostProcessBuild]
+	public static void OnPostProcessBuild( BuildTarget buildTarget, string path)
+	{
+		if(buildTarget == BuildTarget.iOS)
+		{
+			string projectPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+
+			PBXProject pbxProject = new PBXProject();
+			pbxProject.ReadFromFile(projectPath);
+
+			string target = pbxProject.TargetGuidByName("Unity-iPhone");            
+			pbxProject.SetBuildProperty(target, "ENABLE_BITCODE", "NO");
+
+			pbxProject.WriteToFile (projectPath);
+		}
+	}
+}

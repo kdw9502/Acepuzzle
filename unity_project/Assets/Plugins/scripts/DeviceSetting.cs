e175832006e3615d1f830b4c58777f7a59cf54b2
kdw9502ACE <kdw9502@aceproject.com>
2018-04-09 17:18:45 +0900

    [빌드] 시뮬레이터 빌드 설정 추가

--- unity_project/Assets/Plugins/scripts/DeviceSetting.cs
+++ unity_project/Assets/Plugins/scripts/DeviceSetting.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OfferwalldeviceSetting : MonoBehaviour {
+public class DeviceSetting : MonoBehaviour {
 	// Use this for initialization
 	public GameObject offerwallLabel;
+	public GameObject eventLabel;
+	public GameObject noticeLabel;
 	void Start () {
 		#if UNITY_IOS
 			offerwallLabel.SetActive (false);
+		#endif
+		#if IOS_SIMULATOR
+		evnentLabel.SetActive(false);
+		noticeLabel.SetActive(false);
 		#endif
 		#if UNITY_ANDROID
 		offerwallLabel.SetActive(true);
